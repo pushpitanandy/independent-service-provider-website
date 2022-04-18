1c295a07e23b3c4a5db76cd138a4116be8530fa0
pushpitanandy <nandypushpita@gmail.com>
2022-04-18 18:35:28 +0600

    banner added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,11 +1,36 @@
 import React from 'react';
+import { Carousel } from 'react-bootstrap';
+
+import banner1 from '../../../images/banner1.png';
+import banner2 from '../../../images/banner2.png';
 
 const Banner = () => {
     return (
-        <div>
+        <Carousel>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100" height="450px"
+                    src={banner1}
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                    <h3>Want to Immigrate to Canada?</h3>
+                    <p>Get the desired service <br />To fulfil your dream.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100" height="450px"
+                    src={banner2}
+                    alt="Second slide"
+                />
 
-        </div>
+                <Carousel.Caption>
+                    <h3>Want to become a Citizen of Canada?</h3>
+                    <p>Get the desired service<br /> To fulfil your dream.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
     );
 };
-
 export default Banner;
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Button, Card } from 'react-bootstrap';
 
 const Services = () => {
 
@@ -12,6 +13,17 @@ const Services = () => {
     return (
         <div>
             <h2>Services: {services.length}</h2>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the bulk of
+                        the card's content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
         </div>
     );
 };
