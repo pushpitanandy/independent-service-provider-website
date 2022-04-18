facfe1569f68f2268147c0934f46ec1e1fe1739b
pushpitanandy <nandypushpita@gmail.com>
2022-04-18 16:42:28 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './Pages/About/About';
+import Blogs from './Pages/Blogs/Blogs';
 import Home from './Pages/Home/Home/Home';
+import Services from './Pages/Home/Services/Services';
 import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
 import NotFound from './Pages/Shared/NotFound/NotFound';
@@ -12,6 +14,8 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/services" element={<Services></Services>}></Route>
+        <Route path="/blogs" element={<Blogs></Blogs>}></Route>
         <Route path="/about" element={<About></About>}></Route>
         <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import visaIcon from '../../../images/visa-icon.png';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <header>
-            <h2>This is header</h2>
-        </header>
+        <>
+            <Navbar collapseOnSelect expand="lg" sticky='top' bg="light" variant="light">
+                <Container>
+                    <Navbar.Brand as={Link} to="/">
+                        <img src={visaIcon} height='20px' alt="" /> {' '}
+                        Dream Canada</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link as={Link} to="services">Services</Nav.Link>
+                            <Nav.Link as={Link} to="blogs">Blogs</Nav.Link>
+                        </Nav>
+                        <Nav>
+                            <Nav.Link as={Link} to="about">About</Nav.Link>
+                            <Nav.Link as={Link} to="login">
+                                Login
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
     );
 };
 
